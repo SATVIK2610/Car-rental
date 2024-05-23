0562706faeb61b7aad16ca1c95c46fb9d2230792
SATVIK2610 <88959905+SATVIK2610@users.noreply.github.com>
2024-05-23 13:46:55 +0530

    Create Banner.js

---
+++ src/Components/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='banner txt-c flex-col'>
+        <h1>Save big with our cheap car rental!</h1>
+        <h3>Top Airports. Local Suppliers. <span>24/7</span> Support.</h3>
+    </div>
+  )
+}
+
+export default Banner
